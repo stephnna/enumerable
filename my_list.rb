9d249016750fc0e17cc5f9496238a8e94115d762
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-09-23 17:37:41 +0100

    Corrected linter error

--- my_list.rb
+++ my_list.rb
@@ -14,17 +14,17 @@ end
 
 # Test #all?
 list = MyList.new(1, 2, 3, 4)
-list.all? {|e| e < 5}
+list.all? { |e| e < 5 }
 # => true
-list.all? {|e| e > 5}
+list.all? { |e| e > 5 }
 # => false
 
 # # Test #any?
-list.any? {|e| e == 2}
+list.any? { |e| e == 2 }
 # => true
-list.any? {|e| e == 5}
+list.any? { |e| e == 5 }
 # => false
 
 # # Test #filter
-list.filter {|e| e.even?}
+list.filter(&:even?)
 # => [2, 4]
